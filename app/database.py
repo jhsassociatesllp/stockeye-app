6062b79798f631c6644fed8e8094f2bc59eb8061
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2025-12-10 16:50:04 +0530

    Updated Mongo Connection string

--- app/database.py
+++ app/database.py
@@ -9,6 +9,7 @@ MONGO_URI = os.getenv("MONGO_CONNECTION_STRING")
 print(MONGO_URI)
 
 client = MongoClient(MONGO_URI)
+
 db = client["Audit_App"]  # Database name
 users = db["Users"]
 temp_audit_data_collection = db["Audit_Data"]
