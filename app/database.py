e51898c085ced6fb140e3d222ac3376e11b2bea0
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2025-12-10 16:53:46 +0530

    Using Motor asyncio for mongodb connection

--- app/database.py
+++ app/database.py
@@ -1,15 +1,13 @@
 import os
 from dotenv import load_dotenv
-# import motor.motor_asyncio
-from pymongo import MongoClient
+import motor.motor_asyncio
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_CONNECTION_STRING")
 print(MONGO_URI)
 
-client = MongoClient(MONGO_URI)
-
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client["Audit_App"]  # Database name
 users = db["Users"]
 temp_audit_data_collection = db["Audit_Data"]
