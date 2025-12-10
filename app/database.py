4592726f9fedbde8a2d2f813373f0d9ca3f98bbb
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2025-12-10 16:39:04 +0530

    Replaced Motor client with pymongo

--- app/database.py
+++ app/database.py
@@ -1,13 +1,14 @@
 import os
 from dotenv import load_dotenv
-import motor.motor_asyncio
+# import motor.motor_asyncio
+from pymongo import MongoClient
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_CONNECTION_STRING")
 print(MONGO_URI)
 
-client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
+client = MongoClient(MONGO_URI)
 db = client["Audit_App"]  # Database name
 users = db["Users"]
 temp_audit_data_collection = db["Audit_Data"]
