1a3f36515d023e2eec001239d3b2e93a80381d32
Vasu Gadde <vasu.gadde@JHS.LOCAL>
2026-01-13 12:44:27 +0530

    Solving login api issue

--- app/main.py
+++ app/main.py
@@ -175,6 +175,7 @@ async def login(user: UserLogin):
         #         status_code=status.HTTP_401_UNAUTHORIZED
         #     )
         
+        
         logger.info("Getting password and hash")
         password = user.password
         password_hash = db_user.get("password_hash")
